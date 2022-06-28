adda5426ef5d7d9c697da6597523614ca6489901
Sletts <Qopster@gmail.com>
2022-06-28 10:10:55 -0700

    6_27_PracticeProblem

---
+++ 6_27_2022_Coding_Challenge/Problems.cs
@@ -0,0 +1,55 @@
+﻿//Question 1
+string stringLights = Console.ReadLine();
+int lights = Int32.Parse(stringLights);
+
+for(int i = 0; i < lights; i++)
+{
+    int n = Int32.Parse(Console.ReadLine());
+    Console.WriteLine((n + 1) * (n + 2) / 2 - 1);
+}
+//Question 2
+string stringcount = Console.ReadLine();
+int count = Int32.Parse(stringcount);
+string[] firstNames = new string[count];
+string[] secondNames = new string[count];
+for(int i = 0; i < count; i++)
+{
+    string bothNames = Console.ReadLine();
+    string[] words = bothNames.Split(' ');
+    string name1 = words[0];
+    string name2 = words[1];
+    firstNames[i] = name1;
+    secondNames[i] = name2;
+}
+
+string[] answers = new string[count];
+for (int i = 0; i < count; i++)
+{
+    string name1 = firstNames[i];
+    string name2 = secondNames[i];
+    string mainString = "";
+    string checkString = "";
+    if (name1.Length < name2.Length)
+    {
+        mainString = name1;
+        checkString = name2;
+    }
+    else
+    {
+        mainString = name2;
+        checkString = name1;
+    }
+    foreach (char c in mainString)
+    {
+        if (mainString.Count(a => (a == c)) > checkString.Count(a => (a == c)))
+        {
+            answers[i] = "NO";
+        }
+    }
+    if (answers[i] != "NO")
+        answers[i] = "YES";
+}
+foreach (string answer in answers)
+{
+    Console.WriteLine(answer);
+}
